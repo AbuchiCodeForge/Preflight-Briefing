c34cb459347e2eae79739934bb2abcfb67d75a7a
Maduabuchi <udokwu.maduabuchi@gmail.com>
2026-09-11 23:53:56 -0400

    just more skeleton for when I have more time.

---
+++ app/config.py
@@ -0,0 +1,7 @@
+"""
+Configuration and settings for the flight briefer application.
+Environment variables, API limits, timeouts go here.
+"""
+
+API_TIMEOUT_SECONDS = 10
+AVIATION_WEATHER_BASE_URL = "https://aviationweather.gov/api/data"
